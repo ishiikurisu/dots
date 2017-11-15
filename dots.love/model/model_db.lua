765f8514ca08d77194cb54e75aff3cd2c463d00d
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-11-14 23:43:06 -0200

    Loading a board file from the data folder

--- dots.love/model/model_db.lua
+++ dots.love/model/model_db.lua
@@ -1,30 +1,32 @@
 local model_db = { }
 
+-- Gets the data folder path.s
+function model_db.getSource()
+    local cwd = love.filesystem.getWorkingDirectory()
+    return cwd .. '/data'
+end
+
 -- Lists all available options of boards, as described in the
 -- `data/master.txt` file.
 function model_db.list()
-    local options = { }
-    local cwd = love.filesystem.getWorkingDirectory()
-    local target = string.gsub(cwd .. '/data/master.txt', '/', '\\')
-    local lines = love.filesystem.lines(target)
-
-    for line in lines do
-        table.insert(options, line)
-    end
-
-    return options
+    local dataFolder = model_db.getSource()
+    local target = string.gsub(dataFolder .. '/master.txt', '/', '\\')
+    return util.linesFrom(target)
 end
 
--- TODO Actually implement this function
-function model_db.load(src)
+-- Loads the given option from the data folder.
+function model_db.load(option)
+    local src = string.gsub(model_db.getSource() .. '/' .. option, '/', '\\')
+    local lines = util.linesFrom(src)
     local outlet = { }
-    local ndots = 10
+    local ndots = 0
 
-    math.randomseed(os.time())
-    for i = 1, ndots do
-        local x = (math.random() * 1000) % 800
-        local y = (math.random() * 1000) % 600
+    for _, line in pairs(lines) do
+        local fields = util.split(line)
+        local x = tonumber(fields[1])
+        local y = tonumber(fields[2])
         table.insert(outlet, { x = x, y = y })
+        ndots = ndots + 1
     end
 
     return outlet
--- dots.love/util.lua
+++ dots.love/util.lua
@@ -6,4 +6,35 @@ end
 
 util.radius = 5
 
+-- see if the file exists
+function util.fileExists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function util.linesFrom(file)
+    if not util.fileExists(file) then return { } end
+    lines = { }
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+-- Splits a string in a separator
+function util.split(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
 return util
